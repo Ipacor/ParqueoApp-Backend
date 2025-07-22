588d2bd6f681548b3651706258fedae2aeaacdcc
Ipacor <alexisipanaquecordova@gmail.com>
2025-07-22 10:54:03 -0500

    Agregar @NoArgsConstructor a SancionDto para deserialización JSON
    - Corrige error 500 al actualizar sanciones via endpoint PUT
    - Permite que Jackson deserialice correctamente SancionDto desde JSON
    - Resuelve HttpMessageConversionException en SancionController

--- src/main/java/com/parqueo/parkingApp/dto/SancionDto.java
+++ src/main/java/com/parqueo/parkingApp/dto/SancionDto.java
@@ -4,13 +4,17 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.parqueo.parkingApp.model.Sancion.EstadoSancion;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SancionDto {
     private Long id;
 
